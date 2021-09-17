5d4222e6c1f629cd5b1be92c4a191d36f884c636
Trill <ty3torr3s@icloud.com>
2021-09-17 13:21:43 -0400

    finished index and style sheet, started javascript

---
+++ Develop/script.js
@@ -0,0 +1,21 @@
+let startBtn = document.querySelector("#startBtn");
+let answerBtn = document.querySelector("#answerBtn1");
+let answerBtn2 = document.querySelector("#answerBtn2");
+let answerBtn3 = document.querySelector("#answerBtn3");
+let answerBtn4 = document.querySelector("#AnswerBtn4");
+let ansBtn = document.querySelector(".ansBtn");
+let isCorrect = document.querySelector("#correct_wrong");
+let displayForm = document.querySelector("#questiondiv");
+let timerEL = document.querySelector("#countdown");
+let headerEL = document.querySelector("#header");
+let startPage = document.querySelector("#startPage");
+
+const quizQA = [
+    {
+        Question:
+        "What is a useful tool during development and debugging for printing content to the debugger?",
+        choices: [
+            "1. JavaScript",
+        ]
+    }
+]
