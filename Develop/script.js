bbc4784f892aa62dcde0bdc979a6bd8c39e35a12
Trill <ty3torr3s@icloud.com>
2021-09-17 16:51:44 -0400

    added more javascript

--- Develop/script.js
+++ Develop/script.js
@@ -10,12 +10,33 @@ let timerEL = document.querySelector("#countdown");
 let headerEL = document.querySelector("#header");
 let startPage = document.querySelector("#startPage");
 
-const quizQA = [
-    {
-        Question:
-        "What is a useful tool during development and debugging for printing content to the debugger?",
-        choices: [
-            "1. JavaScript",
-        ]
-    }
-]
+const quizQA = [  {
+    Question:
+    "What is a useful tool during development and debugging for printing content to the debugger?",
+    choices: [
+        "1. JavaScript",
+        "2. Terminal/Bash",
+        "3. For Loops",
+        "4. console.log",
+    ],
+    answer: "2",
+},
+{
+    Question: "In the Css, how do you properly style a class?",
+    Choices: [
+        "1. #class {}", "2. .class {}", "3. class {}", "4. *class {}",
+    ],
+    answer: "2",
+
+}
+
+
+
+
+];
+// When User clicks the start button.
+function startGame() {
+    timeStart(event);
+    ansBtn.classList.remove('hide');
+    updatedisplay();
+}
